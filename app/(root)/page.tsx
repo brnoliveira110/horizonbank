7464b56921223fb5e7e18c5686f9fb2d032beff2
Caio Santana <brnoliveirasantana@gmail.com>
2024-06-12 07:09:58 -0300

    Inicio da criação da sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import { HeaderBox } from "@/components/HeaderBox";
+import { RightSidebar } from "@/components/RightSidebar";
 import { TotalBalanceBox } from "@/components/ui/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Caio" };
+  const loggedIn = {
+    firstName: "Caio",
+    lastName: "Santana",
+    email: "caio@email.com",
+  };
 
   return (
     <section className="home">
@@ -22,7 +27,9 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
     </section>
   );
 };
