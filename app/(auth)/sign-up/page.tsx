9f28630e52d031fdaef66fb860da9e7577071d05
Caio Santana <brnoliveirasantana@gmail.com>
2024-06-19 07:25:45 -0300

    criação da página de autenticação

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,14 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <section className="flex-center size-full max-sm:px-6">
+        <AuthForm type="sign-up" />
+      </section>
+    </div>
+  );
 };
 
 export default SignUp;
